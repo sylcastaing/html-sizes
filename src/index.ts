699199f22a2afc0196c974237165f29ca4922394
Sylvain Castaing <scastaing@innovantic.fr>
2021-11-24 14:14:02 +0100

    refactor: refactor generator

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export type SizesValue = `${number}vw` | `${number}px` | `calc(${string})`;
 
 const availableDPR = [1, 2, 3] as const;
 
-type SizesDPR = typeof availableDPR[number];
+export type SizesDPR = typeof availableDPR[number];
 
 const vwRegex = /^([0-9]*)vw$/;
 const pxRegex = /^([0-9]*)px$/;
@@ -18,29 +18,21 @@ export interface SizesOptions {
   dpiCompatibility?: boolean;
 }
 
-function rewriteSizeFromDPR(
+function scaleSizeFromDPR(
   dpr: SizesDPR,
   maxDPR: SizesDPR,
   value: SizesValue
 ): SizesValue {
   const vwMatch = value.match(vwRegex);
 
-  const scaleValue = (matchedValue: string, unit: 'vw' | 'px'): SizesValue => {
-    const scaledValue = parseInt(matchedValue, 10);
-
-    return isNaN(scaledValue)
-      ? value
-      : `${Math.ceil((scaledValue * maxDPR) / dpr)}${unit}`;
-  };
-
   if (vwMatch && vwMatch[1]) {
-    return scaleValue(vwMatch[1], 'vw');
+    return `${Math.ceil((parseInt(vwMatch[1], 10) * maxDPR) / dpr)}vw`;
   }
 
   const pxMatch = value.match(pxRegex);
 
   if (pxMatch && pxMatch[1]) {
-    return scaleValue(pxMatch[1], 'px');
+    return `${Math.ceil((parseInt(pxMatch[1], 10) * maxDPR) / dpr)}px`;
   }
 
   const calcMatch = value.match(calcRegex);
@@ -53,35 +45,33 @@ function rewriteSizeFromDPR(
 }
 
 function getBreakpointDPRMediaQueries(
-  breakpoint: string,
+  breakpoint: string | undefined,
   value: SizesValue,
   dpr: SizesDPR,
   maxDPR: SizesDPR,
   dpiCompatibility: boolean = false
 ) {
-  const rewriteValue = rewriteSizeFromDPR(dpr, maxDPR, value);
+  const scaledValue = scaleSizeFromDPR(dpr, maxDPR, value);
 
-  const mediaQueries = [
-    `(min-resolution: ${dpr}dppx) and ${breakpoint} ${rewriteValue}`,
-    `(-webkit-min-device-pixel-ratio: ${dpr}) and ${breakpoint} ${rewriteValue}`,
-  ];
-
-  if (dpiCompatibility) {
-    return [
-      ...mediaQueries,
-      `(min-resolution: ${96 * dpr}dpi) and ${breakpoint} ${rewriteValue}`,
-    ].join(', ');
-  } else {
-    return mediaQueries.join(', ');
-  }
+  return [
+    `(min-resolution: ${dpr}dppx)`,
+    `(-webkit-min-device-pixel-ratio: ${dpr})`,
+    dpiCompatibility ? `(min-resolution: ${96 * dpr}dpi)` : '',
+  ]
+    .filter((mediaQuery) => !!mediaQuery)
+    .map(
+      (mediaQuery) =>
+        `${mediaQuery}${breakpoint ? ` and ${breakpoint} ` : ' '}${scaledValue}`
+    )
+    .join(', ');
 }
 
 function getBreakpointValue(
-  breakpoint: string,
   value: SizesValue,
-  { maxDPR, dpiCompatibility }: SizesOptions
+  { maxDPR, dpiCompatibility }: SizesOptions,
+  breakpoint?: string
 ) {
-  const breakPointValue = `${breakpoint} ${value}`;
+  const breakPointValue = breakpoint ? `${breakpoint} ${value}` : value;
 
   if (maxDPR) {
     const capedDPRBreakpoints = availableDPR
@@ -105,67 +95,19 @@ function getBreakpointValue(
   }
 }
 
-function getDefaultValueDPRMediaQueries(
-  value: SizesValue,
-  dpr: SizesDPR,
-  maxDPR: SizesDPR,
-  dpiCompatibility: boolean = false
-) {
-  const rewriteValue = rewriteSizeFromDPR(dpr, maxDPR, value);
-
-  const mediaQueries = [
-    `(min-resolution: ${dpr}dppx) ${rewriteValue}`,
-    `(-webkit-min-device-pixel-ratio: ${dpr})  ${rewriteValue}`,
-  ];
-
-  if (dpiCompatibility) {
-    return [
-      ...mediaQueries,
-      `(min-resolution: ${96 * dpr}dpi) ${rewriteValue}`,
-    ].join(', ');
-  } else {
-    return mediaQueries.join(', ');
-  }
-}
-
-function getDefaultValue(
-  value: SizesValue,
-  { maxDPR, dpiCompatibility }: SizesOptions
-) {
-  if (maxDPR) {
-    const capedDPRBreakpoints = availableDPR
-      .filter((dpr) => dpr > maxDPR)
-      .map((dpr) =>
-        getDefaultValueDPRMediaQueries(value, dpr, maxDPR, dpiCompatibility)
-      )
-      .join(',');
-
-    return capedDPRBreakpoints.length > 0
-      ? `${capedDPRBreakpoints}, ${value}`
-      : value;
-  } else {
-    return value;
-  }
-}
-
 function generateSizes(
   params: SizesParams,
   options: SizesOptions = {}
 ): string {
   const { default: receivedDefaultValue, ...breakpoints } = params;
 
-  const breakpointsValue = Object.keys(breakpoints).reduce(
-    (acc, breakpoint, index) => {
-      return `${index === 0 ? acc : `${acc}, `}${getBreakpointValue(
-        breakpoint,
-        breakpoints[breakpoint],
-        options
-      )}`;
-    },
-    ''
-  );
+  const breakpointsValue = Object.keys(breakpoints)
+    .map((breakpoint) =>
+      getBreakpointValue(breakpoints[breakpoint], options, breakpoint)
+    )
+    .join(`, `);
 
-  const defaultValue = getDefaultValue(receivedDefaultValue, options);
+  const defaultValue = getBreakpointValue(receivedDefaultValue, options);
 
   return breakpointsValue.length > 0
     ? `${breakpointsValue}, ${defaultValue}`
--- test/index.test.ts
+++ test/index.test.ts
@@ -32,7 +32,7 @@ describe('generateSizes', () => {
         { maxDPR: 2 }
       )
     ).toBe(
-      '(min-resolution: 3dppx) and (max-width: 340px) 67vw, (-webkit-min-device-pixel-ratio: 3) and (max-width: 340px) 67vw, (max-width: 340px) 100vw, (min-resolution: 3dppx) and (min-width: 400px) 24px, (-webkit-min-device-pixel-ratio: 3) and (min-width: 400px) 24px, (min-width: 400px) 35px, (min-resolution: 3dppx) 34vw, (-webkit-min-device-pixel-ratio: 3)  34vw, 50vw'
+      '(min-resolution: 3dppx) and (max-width: 340px) 67vw, (-webkit-min-device-pixel-ratio: 3) and (max-width: 340px) 67vw, (max-width: 340px) 100vw, (min-resolution: 3dppx) and (min-width: 400px) 24px, (-webkit-min-device-pixel-ratio: 3) and (min-width: 400px) 24px, (min-width: 400px) 35px, (min-resolution: 3dppx) 34vw, (-webkit-min-device-pixel-ratio: 3) 34vw, 50vw'
     );
   });
 
@@ -45,7 +45,50 @@ describe('generateSizes', () => {
         { maxDPR: 2 }
       )
     ).toBe(
-      '(min-resolution: 3dppx) calc((100vw - 20px) * 2 / 3), (-webkit-min-device-pixel-ratio: 3)  calc((100vw - 20px) * 2 / 3), calc(100vw - 20px)'
+      '(min-resolution: 3dppx) calc((100vw - 20px) * 2 / 3), (-webkit-min-device-pixel-ratio: 3) calc((100vw - 20px) * 2 / 3), calc(100vw - 20px)'
     );
+  });
+
+  it('dpr compatibility', () => {
+    expect(
+      generateSizes(
+        {
+          '(max-width: 500px)': '100vw',
+          default: '500px',
+        },
+        {
+          maxDPR: 2,
+          dpiCompatibility: true,
+        }
+      )
+    ).toBe(
+      '(min-resolution: 3dppx) and (max-width: 500px) 67vw, (-webkit-min-device-pixel-ratio: 3) and (max-width: 500px) 67vw, (min-resolution: 288dpi) and (max-width: 500px) 67vw, (max-width: 500px) 100vw, (min-resolution: 3dppx) 334px, (-webkit-min-device-pixel-ratio: 3) 334px, (min-resolution: 288dpi) 334px, 500px'
+    );
+  });
+
+  it('unmatched value', () => {
+    expect(
+      generateSizes(
+        {
+          // @ts-ignore
+          '(max-width: 500px)': '100%',
+          default: '500px',
+        },
+        { maxDPR: 2 }
+      )
+    ).toBe(
+      '(min-resolution: 3dppx) and (max-width: 500px) 100%, (-webkit-min-device-pixel-ratio: 3) and (max-width: 500px) 100%, (max-width: 500px) 100%, (min-resolution: 3dppx) 334px, (-webkit-min-device-pixel-ratio: 3) 334px, 500px'
+    );
+  });
+
+  it('maxDPR 3 no effect', () => {
+    expect(
+      generateSizes(
+        {
+          default: '500px',
+        },
+        { maxDPR: 3 }
+      )
+    ).toBe('500px');
   });
 });
